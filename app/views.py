8fcc6340b55afe79811fd07849d6e795d0bb2ff8
hadi <hadi27767@gmail.com>
2023-09-04 23:00:10 -0700

    Remove the Errors

--- app/views.py
+++ app/views.py
@@ -192,38 +192,47 @@ def signup_page():
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == "POST":
-        # product= request.json  # Get the JSON data from the request
-        # u_name= product.get('u_name')
-        # u_email= product.get('u_email')
-        # u_details= product.get('u_details')
-        # password= product.get('password')
-        u_name= request.form.get('username')
-        u_email= request.form.get('email')
-        u_details= request.form.get('details')
-        password= request.form.get('password')
-        cpassword= request.form.get('cpassword')
-        if password == cpassword:
-            existing_user = User.query.filter_by(u_email=u_email).first()
-            if existing_user:
-                flash("User Email already Found..! Try with another email address", "danger")
-                return redirect(url_for("signup"))
-            stmt = insert(User).values(u_name=u_name, u_email=u_email, u_details=u_details, password=password)
-            db.session.execute(stmt)
-            db.session.commit()
-            session["user_id"] = u_email
-            flash("You are registered successfully", "success")
-            return redirect(url_for("facesetup"))
-        else:
-            flash("Passwords donot Match", "danger")
+        u_name = request.form.get('username')
+        u_email = request.form.get('email')
+        u_details = request.form.get('details')
+        password = request.form.get('password')
+
+        
+        existing_user = User.query.filter_by(u_email=u_email).first()
+        if existing_user:
+            flash("User Email already Found..! Try with another email address", "danger")
             return redirect(url_for("signup"))
+
+        # Fetch the device's IP address from the form data
+        device_ip = request.form.get('ipAddress')
+        print("device ip is : ", device_ip)
+
+        # Create a new user and store the MAC address
+        new_user = User(u_name=u_name, u_email=u_email, u_details=u_details, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+            # Store the MAC address associated with the user
+        
+        new_mac_address = MacAddress(mac_address=device_ip, user_id=new_user.u_id)
+        db.session.add(new_mac_address)
+        db.session.commit()
+            
+        session["user_id"] = u_email
+        flash("You are registered successfully", "success")
+        return redirect("/facesetup")
+       
+        
     else:
-        flash("invalid request", "danger")
+        flash("Passwords don't Match", "danger")
         return redirect(url_for("signup"))
+
 
 
 @app.route("/home")
 def home():
     return render_template("home.html")
+
 
 @app.route("/login")
 def login_page():
@@ -235,106 +244,53 @@ def login():
         # user_data = request.json  # Get the JSON data from the request
         username = request.form.get('username')
         password = request.form.get('password')
-        ip_address = request.form.get('ipAddress')  
+        ip_address = request.remote_addr
         print("the Ip_address is : ", ip_address)
         user = User.query.filter_by(u_name=username).first()
+        print("the user id is: ", user.u_id)
         if user and user.password == password:
             existing_mac_address = MacAddress.query.filter_by(user_id=user.u_id, mac_address=ip_address).first()
             if existing_mac_address:
-                new_mac_address = MacAddress(user_id=user.u_id, mac_address=ip_address)
-                db.session.add(new_mac_address)
-                db.session.commit()
-                
-                flash("Device registered successfully.", "success")
+                flash("Device Logged in successfully.", "success")
                 login_user(user)
                 session['ip_address'] = ip_address
                 session["user_id"] = user.u_id
-                return redirect(url_for("facereg"))
-                
+                session["authenticated"] = False
+                return redirect(url_for("home"))
+               
             else:
                 flash("We Observe that you are trying to login with new ip adress", "info")
                 return redirect(url_for("login"))
-              
+            
         else:
             flash("You have supplied invalid login credentials!", "danger")
             return redirect(url_for("login"))
     else:
         return jsonify({"message": "Invalid Request Method"}), 401, {'Content-Type': 'application/json'}
-    # if request.method == 'POST':
-    #     # user_data = request.json  # Get the JSON data from the request
-    #     # username = user_data.get('username')
-    #     # password = user_data.get('password')
-    #     username = request.form.get("username")
-    #     password = request.form.get("password")
-    #     user = User.query.filter_by(u_name=username).first()
-    #     if user and user.password == password:
-    #         mac_address = get('https://api.ipify.org').text
-    #         print("mac address is: ", mac_address)
-
-    #         existing_mac_address = MacAddress.query.filter_by(user_id=user.u_id, mac_address=mac_address).first()
-    #         if existing_mac_address:
-    #             flash("Device already registered.", "info")
-    #             return redirect(url_for("login"))
-    #         else:
-    #             new_mac_address = MacAddress(user_id=user.u_id, mac_address=mac_address)
-    #             db.session.add(new_mac_address)
-    #             db.session.commit()
-    #             flash("Device registered successfully.", "success")
-    #             login_user(user)
-    #             return redirect(url_for("login_2fa"))
-    #     else:
-    #         flash("You have supplied invalid login credentials!", "danger")
-    #         return redirect(url_for("login"))
-    # else:
-    #     return jsonify({"message": "Invalid Request Method"}), 401
 
 
-def get_mac_address():
-    pythoncom.CoInitialize() 
-    c = wmi.WMI()
-    wifi_interface = None
-
-    for interface in c.Win32_NetworkAdapter():
-        if interface.NetConnectionID and "Wi-Fi" in interface.NetConnectionID:
-            print("Detected Wi-Fi interface:", interface.NetConnectionID)
-            wifi_interface = interface
-            break
-
-    if wifi_interface:
-        address = wifi_interface.MACAddress
-        return address
-    else:
-        return None
-
-def store_mac_address(user_id, mac_address):
-    mac_entry = MacAddress(user_id=user_id, mac_address=mac_address)
-    db.session.add(mac_entry)
-    db.session.commit()
     
-    
-    
-    
-@app.route("/address")
-def index():
-  """Retrieves the MAC address of the client device and returns it as a JSON response."""
-#   ip_address = flask.request.remote_addr
-  interface_names = netifaces.interfaces()
+# @app.route("/address")
+# def index():
+#   """Retrieves the MAC address of the client device and returns it as a JSON response."""
+# #   ip_address = flask.request.remote_addr
+#   interface_names = netifaces.interfaces()
 
-  chosen_interface = interface_names[0]
-  mac_address = netifaces.ifaddresses(chosen_interface)[netifaces.AF_LINK][0]['addr']
-  return jsonify({"mac_address": mac_address})
+#   chosen_interface = interface_names[0]
+#   mac_address = netifaces.ifaddresses(chosen_interface)[netifaces.AF_LINK][0]['addr']
+#   return jsonify({"mac_address": mac_address})
 
 
-@app.route("/cookie", methods=['GET'])
-def get_cookie():
-    if current_user.is_authenticated:
-        mac_address = session.get('mac_address')
-        if mac_address:
-            return jsonify(cookie_value= mac_address)
-        else:
-            return jsonify(message="Cookie not found")
-    else:
-        return jsonify(message="User is not authenticated")
+# @app.route("/cookie", methods=['GET'])
+# def get_cookie():
+#     if current_user.is_authenticated:
+#         mac_address = session.get('mac_address')
+#         if mac_address:
+#             return jsonify(cookie_value= mac_address)
+#         else:
+#             return jsonify(message="Cookie not found")
+#     else:
+#         return jsonify(message="User is not authenticated")
     
     
     
@@ -356,7 +312,6 @@ def login_2fa_form():
     if pyotp.TOTP(secret).verify(otp):
         session["authenticated"] = True
         response_data = {"success": True, "message": "The TOTP 2FA token is valid"}
-        
         
     else:
         response_data = {"success": False, "message": "You have supplied an invalid 2FA token!"}
@@ -467,27 +422,39 @@ def facesetup():
             if not existing_image_paths:
                 flash("No Other Users Exist", "success")
                 return redirect("/login")
-
-            for existing_image_path in existing_image_paths:
-                try:
-                    existing_image = face_recognition.load_image_file(existing_image_path)
-                    existing_face_encoding = face_recognition.face_encodings(existing_image)[0]
-                except:
-                    flash("Other image not cleared", "success")
-                    return render_template("face.html")
-
-                match = face_recognition.compare_faces([existing_face_encoding], bill_face_encoding)
-
-                if match[0]:
+            
+            flag = 0 
+            if len(existing_image_paths) >= 1:
+                for existing_image_path in existing_image_paths:
+                    print("existing_image_path: ",  existing_image_path)
+                    try:
+                       existing_image = face_recognition.load_image_file(existing_image_path)
+                       existing_face_encoding = face_recognition.face_encodings(existing_image)[0]
+                    except:
+                       flash("Other image not cleared", "success")
+                       return render_template("face.html")
+                    print("Image of bill: ",  bill_face_encoding)
+                    print("existing image: ", existing_face_encoding)
+                    
+                    
+                    match = face_recognition.compare_faces([existing_face_encoding], bill_face_encoding,  tolerance=0.4)
+                    print("the match is: ",match[0])
+                    
+                    if match[0]:
+                        flag = 1
+                        print("flag", flag)
+                        break  
+                           
+                if flag:
                     flash("User Image already exists with another username", "danger")
                     os.remove(new_image_path)
                     return render_template("face.html")
-
-            return redirect("/login") # If the loop completes without finding a match, you can proceed here
-
+                else:
+                    return redirect("/login") 
+                    
         else:
             flash("User not found", "danger")
-
+            
         return redirect("/home")
 
     else:
@@ -569,53 +536,110 @@ def get_facereg():
 def facereg():
     session.clear()
     if request.method == "POST":
+        # Get the user input image
         encoded_image = (request.form.get("pic") + "==").encode('utf-8')
-        u_name = request.form.get("name") 
-        
-        user = User.query.filter_by(u_name=u_name).first()  
-        if not user:
-            flash("No Such User Recognized", "danger")
-            return redirect(url_for("facereg"))
 
-        id_ = user.u_id
+        # Decode and save the image in the 'unknown' folder
         compressed_data = zlib.compress(encoded_image, 9)
         uncompressed_data = zlib.decompress(compressed_data)
         decoded_data = b64decode(uncompressed_data)
 
-        # Save the image file
-        new_image_path = f'./app/static/face/unknown/{id_}.jpg'
+        new_image_path = f'./app/static/face/unknown/1.jpg'
         with open(new_image_path, 'wb') as new_image_handle:
             new_image_handle.write(decoded_data)
 
         try:
-            image_of_bill = face_recognition.load_image_file(f'./app/static/face/{id_}.jpg')
-        except:
-            flash("Not set face recognition yet", "danger")
-            return render_template("camera.html")
-
-        bill_face_encoding = face_recognition.face_encodings(image_of_bill)[0]
-
-        unknown_image = face_recognition.load_image_file(new_image_path)
-        try:
+            unknown_image = face_recognition.load_image_file(new_image_path)
             unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
         except:
             flash("The Image is Not Clear", "danger")
             return render_template("camera.html")
 
-        # Compare faces
-        results = face_recognition.compare_faces([bill_face_encoding], unknown_face_encoding)
+        # Loop through all known faces in the /face folder and find a match
+        known_faces_directory = './app/static/face/'
+        matching_user_id = None
 
-        if results[0]:
-            # Query the user by username "swa" and set the user_id in the session
-            username = User.query.filter_by(u_name=u_name).first()
-            session["user_id"] = username.u_id
-            return redirect("/login/2fa/")
+        for filename in os.listdir(known_faces_directory):
+            print("List is:",  os.listdir(known_faces_directory))
+            print("filename",filename)
+            if filename.lower().endswith('.jpg'):
+                try:
+                    known_image = face_recognition.load_image_file(os.path.join(known_faces_directory, filename))
+                    known_face_encoding = face_recognition.face_encodings(known_image)[0]
+                    
+                    result = face_recognition.compare_faces([known_face_encoding], unknown_face_encoding, tolerance=0.4)
+                    print("result is ", result[0])
+                    if result[0]:
+                        # Match found, retrieve the associated user ID
+                        user_id = int(filename.split('.')[0])  # Assuming filenames are user IDs
+                        matching_user_id = user_id
+                        print(user_id)
+                        break  # Exit the loop after finding a match
+                except:
+                    continue  # Ignore any errors and continue searching
+
+        if matching_user_id is not None:
+            # Successful face recognition, retrieve the user from the database
+            user = User.query.get(matching_user_id)
+            if user:
+                login_user(user)
+                session["user_id"] = user.u_id
+                session["authenticated"] = False
+                flash("You are successsfully logged in", "success")
+                return redirect("/home")
+            else:
+                flash("User not found in the database", "danger")
         else:
-            flash("Incorrect Face", "danger")
-            return render_template("camera.html")
+            flash("Face not recognized as any registered user", "danger")
 
-    else:
-        return render_template("camera.html")
+    return render_template("camera.html")
+    
+# def facereg():
+#     session.clear()
+#     if request.method == "POST":
+#         encoded_image = (request.form.get("pic") + "==").encode('utf-8')
+       
+        
+
+#         id_ = user.u_id
+#         compressed_data = zlib.compress(encoded_image, 9)
+#         uncompressed_data = zlib.decompress(compressed_data)
+#         decoded_data = b64decode(uncompressed_data)
+
+#         # Save the image file
+#         new_image_path = f'./app/static/face/unknown/{id_}.jpg'
+#         with open(new_image_path, 'wb') as new_image_handle:
+#             new_image_handle.write(decoded_data)
+
+#         try:
+#             image_of_bill = face_recognition.load_image_file(f'./app/static/face/{id_}.jpg')
+#         except:
+#             flash("Not set face recognition yet", "danger")
+#             return render_template("camera.html")
+
+#         bill_face_encoding = face_recognition.face_encodings(image_of_bill)[0]
+
+#         unknown_image = face_recognition.load_image_file(new_image_path)
+#         try:
+#             unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+#         except:
+#             flash("The Image is Not Clear", "danger")
+#             return render_template("camera.html")
+
+#         # Compare faces
+#         results = face_recognition.compare_faces([bill_face_encoding], unknown_face_encoding)
+
+#         if results[0]:
+#             # Query the user by username "swa" and set the user_id in the session
+#             username = User.query.filter_by(u_name=u_name).first()
+#             session["user_id"] = username.u_id
+#             return redirect("/login/2fa/")
+#         else:
+#             flash("Incorrect Face", "danger")
+#             return render_template("camera.html")
+
+#     else:
+#         return render_template("camera.html")
 
 
 
